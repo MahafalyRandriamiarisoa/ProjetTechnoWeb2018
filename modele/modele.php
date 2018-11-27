a0ee5225b51d3808bfcba88819ec74fefe211e00
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2018-11-27 22:03:41 +0100

    Changement du mcd pour le numéro de telClient 10 à 15 char, ajouts de fichier de données de test, modification sur le modèle

--- modele/modele.php
+++ modele/modele.php
@@ -1,14 +1,22 @@
 <?php
 
 function getConnect(){
-    require_once('connect.php');
+		require_once('connect.php');
 
-    try{
-        $connexion = new PDO('mysql:host='.SERVEUR.';dbname='.BDD, USER, PASSWORD);
-        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $connexion->query("SET NAMES UTF8");
-        return $connexion;
-      }catch(PDOException $e){
-        echo 'Il y a eu une erreur de connection : ' . $e->getMessage();
-      }
+		try{
+				$connexion = new PDO('mysql:host='.SERVEUR.';dbname='.BDD, USER, PASSWORD);
+				$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$connexion->query("SET NAMES UTF8");
+				return $connexion;
+			}catch(PDOException $e){
+				echo 'Il y a eu une erreur de connexion : ' . $e->getMessage();
+			}
+}
+
+function showAll(){
+	$connexion = getConnect();
+	$requete = "SELECT * FROM CLIENT";
+	$resultat = $connexion->query($requete);
+	$resultat->setFetchMode(PDO::FETCH_OBJ);
+	return $resultat->fetchAll();
 }

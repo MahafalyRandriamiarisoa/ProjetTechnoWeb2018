5d897c1407b271a5882f4bf60ff3ef24c18db02a
MahafalyRandriamiarisoa <mahafaly.randriamiarisoa@etu.univ-orleans.fr>
2018-11-28 18:31:24 +0100

    minorbug

--- controleur/controleur.php
+++ controleur/controleur.php
@@ -4,23 +4,28 @@
 
 /**  
  * Fonction pour le contrôle de la toute première interface, l'interface de connexion
- *
+ *@param message qui potentiellement un message inquant une erreur
  */
-    function CtlInterfaceLogin(){ 
-        AfficherInterfaceLogin();
+    function CtlInterfaceLogin($msg){ 
+        AfficherInterfaceLogin($msg);
             }
+   
 
 /**
  * Fonction pour le contrôle de l'Acceuil
  * 
  * il s'agit de l'interface qui suit l'identification,
- * On y récupere la categorie de l'employé ainsi que 
+ *  
  */           
     function CtlAcceuil($login,$mdp){
-        if(!emty($login) && !enmpty($mdp)){
+        if(!emty($login) && !empty($mdp)){
             $employe=checkLogin($login,$mdp);
             if(!empty($employe){
                 AfficherAcceuil(checkLogin($employe,$employe[0]->CATEGORIE);
+            }else{
+                AfficherInterfaceLogin("Identifiants Incorrectes");
             }
+        }else{
+            AfficherInterfaceLogin("Identifiants Incorrectes");
         }
     }

592bb4ea5f965ec1e370c896ee4e64cd1d415622
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2018-11-28 18:51:03 +0100

    Changements mineurs.

--- banque.php
+++ banque.php
@@ -9,11 +9,6 @@
             CtlAfficherInterfaceLogin();
         }
 
-<<<<<<< HEAD
-    }
-    catch(Exception $e1){
-=======
     }catch(Exception $e1){
->>>>>>> be76b2f12de14de6727ab55a3c905ab6db8ab302
         echo $e1->getMessage();
     }

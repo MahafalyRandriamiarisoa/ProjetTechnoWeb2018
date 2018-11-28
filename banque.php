136607eb15b8702559002e41ebc0d2f9fa6e28a5
MahafalyRandriamiarisoa <mahafaly.randriamiarisoa@etu.univ-orleans.fr>
2018-11-28 18:43:13 +0100

    correction d'erreur dans banque.php

--- banque.php
+++ banque.php
@@ -2,10 +2,14 @@
     try{
         if(isset($_POST['connexion'])){
             CtlAcceuil($_POST['identifiant'],$_POST['motDePasse']);
-        }else{
+        }elseif(isset($_POST['bouton1'])){
+
+        }
+        else{
             CtlAfficherInterfaceLogin();
         }
 
-    }catch(PDOException e1){
-        echo e1->getMessage();
+    }
+    catch(Exception $e1){
+        echo $e1->getMessage();
     }

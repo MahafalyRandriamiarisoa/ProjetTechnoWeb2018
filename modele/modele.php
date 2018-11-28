c518b80034876529d4dfcd653f8f99b33b6aa289
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2018-11-28 17:14:31 +0100

    Ajout des fonctions checkLogin, checkClient, rechercherClient dans le modele.php

--- modele/modele.php
+++ modele/modele.php
@@ -2,7 +2,6 @@
 
 function getConnect(){
 		require_once('connect.php');
-
 		try{
 				$connexion = new PDO('mysql:host='.SERVEUR.';dbname='.BDD, USER, PASSWORD);
 				$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -16,6 +15,30 @@ function getConnect(){
 function showAll(){
 	$connexion = getConnect();
 	$requete = "SELECT * FROM CLIENT";
+	$resultat = $connexion->query($requete);
+	$resultat->setFetchMode(PDO::FETCH_OBJ);
+	return $resultat->fetchAll();
+}
+
+function checkLogin($login, $mdp){
+	$connexion = getConnect();
+	$requete = "SELECT * FROM EMPLOYE WHERE '$login' = login AND '$mdp' = mdp";
+	$resultat = $connexion->query($requete);
+	$resultat->setFetchMode(PDO::FETCH_OBJ);
+	return $resultat->fetchAll();
+}
+
+function checkClient($idClient){
+	$connexion = getConnect();
+	$requete = "SELECT * FROM CLIENT WHERE numClient = $idClient";
+	$resultat = $connexion->query($requete);
+	$resultat->setFetchMode(PDO::FETCH_OBJ);
+	return $resultat->fetchAll();
+}
+
+function rechercherClient($nom, $dateNaissance){
+	$connexion = getConnect();
+	$requete = "SELECT * FROM CLIENT WHERE nom = '$nom' AND dateDeNaissance = STR_TO_DATE('$dateNaissance', '%Y-%m-%d')";
 	$resultat = $connexion->query($requete);
 	$resultat->setFetchMode(PDO::FETCH_OBJ);
 	return $resultat->fetchAll();

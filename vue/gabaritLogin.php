f0cf8e2b73bbaa1bf89f0f07016870f8fc185d76
Vali00 <45363371+Vali00@users.noreply.github.com>
2018-12-02 18:16:57 +0100

    Update and rename gabarit.php to gabaritLogin.php
    Ajout gabarit de l'interface d'identification

---
+++ vue/gabaritLogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+      <title>Ma page</title>
+      <meta charset="utf-8">
+	  <link rel="stylesheet"  href="vue/style/styles.css" />
+	  
+    </head>
+    
+	<body>
+	<header>
+		<h1> Banque 3.0 </h1>
+	</header>
+	<div>
+		<form id="formLogin" method="post" action="banque.php">
+			<fieldset>
+				
+				<p><label>Login : </label><input type="text" name="identifiant"/></p>
+				<p><label>Mot de passe : </label><input type="text" name="motDePasse"/></p>
+				<p><input type="submit"  name="connexion" value="Se connecter"/></p>
+			
+			</fieldset>
+			<?php echo $contenu ?>
+		</form>
+	</div>
+	</body>
+</html>

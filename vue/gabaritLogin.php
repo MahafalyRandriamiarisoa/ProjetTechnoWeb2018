9fb1fe53e278432db8a7938e4f4d3e01aa335774
Vali00 <45363371+Vali00@users.noreply.github.com>
2018-12-05 15:49:47 +0100

    Update gabaritLogin.php

--- vue/gabaritLogin.php
+++ vue/gabaritLogin.php
@@ -16,7 +16,7 @@
 			<fieldset>
 				
 				<p><label>Login : </label><input type="text" name="identifiant"/></p>
-				<p><label>Mot de passe : </label><input type="text" name="motDePasse"/></p>
+				<p><label>Mot de passe : </label><input type="password" name="motDePasse"/></p>
 				<p><input type="submit"  name="connexion" value="Se connecter"/></p>
 			
 			</fieldset>

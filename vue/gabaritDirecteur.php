02e4ea4b4877ab89d3b3fcfe6ada23dc9f535a68
Vali00 <45363371+Vali00@users.noreply.github.com>
2018-12-07 13:25:47 +0100

    Le gabarit du directeur

---
+++ vue/gabaritDirecteur.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+      <title>Ma page</title>
+      <meta charset="utf-8">
+	  <link rel="stylesheet"  href="vue/style/styles.css" />
+	  
+    </head>
+    
+	<body>	
+		<header>
+			<p>Connexion en tant que :<?php echo $contenuHeader;?></p>
+		</header>
+		<aside>
+			<form id="f1" method="post" action="banque.php">
+			<?php echo '<p><input type="text" name="categorie" value="Directeur"style="display:none" /></p>' ?>
+				<fieldset>
+					<legend> Actions </legend>
+					<p><label>Sélectionner une action à réaliser :<label></p>
+					<p>
+						<select name="action">
+							<option value="modifId"/>Modification des identifiants</option>
+							<option value="modifMotif"/>Modification de la liste des comptes et contrats</option>
+							<option value="modifPiece"/>Modification de la liste des pièces à fournir</option>
+							<option value="stat"/>Statistiques de la banque</option>
+						</select>
+					</p>
+					<p><input type="submit" name="validerDirecteur" value="Valider"/></p>
+				</fieldset>
+			</form>
+		</aside>
+		<div>
+			<?php echo $contenuInterface;
+				  echo $contenuBis;?>
+		</div>
+	</body>
+</html>

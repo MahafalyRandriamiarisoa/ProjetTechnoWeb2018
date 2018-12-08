ab84fb48ab3a0a7f3124fd0245ce8e5f2e79a8cc
Touliman <samir.toularhmine@gmail.com>
2018-12-08 18:57:24 +0100

    Modification sur la fonction ajouterRDV dans le modele.php

--- modele/modele.php
+++ modele/modele.php
@@ -411,7 +411,7 @@ function resilierClient($numClient){
 
 function ajouterRDV($idEmploye, $idMotif, $numClient, $dateHeureRDV){
 	$connexion = getConnect();
-	$requete = "INSERT INTO RENDEZVOUS VALUES (0, $idMotif, $numClient, STR_TO_DATE('$dateHeureRDV', '%d/%m/%Y/%H'))";
+	$requete = "INSERT INTO RENDEZVOUS VALUES (0, $idEmploye, $idMotif, $numClient, STR_TO_DATE('$dateHeureRDV', '%d/%m/%Y/%k'))";
 	$connexion->query($requete);
 }
 

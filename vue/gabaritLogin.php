d891f5c96046ff789859622cfb0652f82f5359e6
MahafalyRandriamiarisoa <mahafaly.randriamiarisoa@etu.univ-orleans.fr>
2018-12-17 20:10:51 +0100

    css: bouton de connexion

--- vue/gabaritLogin.php
+++ vue/gabaritLogin.php
@@ -17,7 +17,7 @@
 				<fieldset id="fieldsetLogin">
 						<p><label class="divLabel">Login : </label><br><input type="text" name="identifiant"/></p><br/>
 						<p><label class="divLabel">Mot de passe : </label><br><input type="password" name="motDePasse"/></p>
-						<p><label class="divLabel"></label><input type="submit"  name="connexion" value="Se connecter"/></p>
+						<p><label class="divLabel"></label><input class="btn" type="submit"  name="connexion" value="Se connecter"/></p>
 				</fieldset>
 				<?php echo $contenu ?>
 			</form>

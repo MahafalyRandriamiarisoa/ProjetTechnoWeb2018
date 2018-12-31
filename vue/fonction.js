d156a5ef809e7a46094fb1abbdd9cc78313fee39
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2018-12-31 23:03:37 +0100

    Ajout d'une fonction pour obtenir la date du jour en javascript dans fonction.js

--- vue/fonction.js
+++ vue/fonction.js
@@ -25,11 +25,11 @@ function getCurrDate(){
 	var dd = today.getDate();
 	var mm = today.getMonth()+1; //January is 0!
 	var yyyy = today.getFullYear();
-	if(dd<10){
-		dd='0'+dd
+	if(dd < 10){
+		dd='0'+dd;
 	} 
-	if(mm<10){
-		mm='0'+mm
+	if(mm < 10){
+		mm = '0' + mm;
 	} 
 
 	return yyyy+'-'+mm+'-'+dd;
